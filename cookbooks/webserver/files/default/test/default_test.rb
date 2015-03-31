5acfa42c46b4f90ea498b5392916d07a50303977
Udaypal Aarkoti <uaarkoti@gmail.com>
2015-03-31 11:01:45 -0700

    Breaking builds

--- cookbooks/webserver/files/default/test/default_test.rb
+++ cookbooks/webserver/files/default/test/default_test.rb
@@ -4,7 +4,7 @@ include ::MiniTest::Chef::Context
 include ::MiniTest::Chef::Resources
 
 class TestNginx < MiniTest::Chef::Spec
-  it "listens on port 80" do
-      TCPSocket.open("localhost", 80)
+  it "listens on port 81" do
+      TCPSocket.open("localhost", 81)
   end
 end
